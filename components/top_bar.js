eeeca130958dd86ab91bc307ea680564596418f8
Patton <pyin@wesleyan.edu>
2023-11-17 01:10:22 -0500

    styling (issue #5)

--- components/top_bar.js
+++ components/top_bar.js
@@ -4,56 +4,62 @@ import { useAuth } from "../AuthContext";
 import Logo from "./logo";
 
 const TopBar = () => {
-    const { username, setUsername, isLogin, setLogin, setSection } = useAuth();
+  const { username, setUsername, isLogin, setLogin, setSection } = useAuth();
 
-    const handleLogout = () => {
-        setUsername(null);
-        setLogin(false);
-        setSection('Login');
-    }
+  const handleLogout = () => {
+    setUsername(null);
+    setLogin(false);
+    setSection("Login");
+  };
 
-    return (
-        <View style={styles.topbar}>
-            <Logo style={styles.logo} />
-            <View style={styles.gap} />
-            {isLogin ?
-                <View>
-                    <Text style={styles.login}>Welcome, {username}</Text>
-                    <TouchableOpacity style={styles.button} onPress={handleLogout}>
-                        <Text style={styles.buttonText}> Logout </Text>
-                    </TouchableOpacity>
-                </View>
-                :
-                <TouchableOpacity style={styles.button} onPress={() => { setSection('Register') }}>
-                    <Text style={styles.buttonText}> Register </Text>
-                </TouchableOpacity>}
+  return (
+    <View style={styles.topbar}>
+      <Logo style={styles.logo} />
+      <View style={styles.gap} />
+      {isLogin ? (
+        <View>
+          <Text style={styles.login}>Welcome, {username}</Text>
+          <TouchableOpacity style={styles.button} onPress={handleLogout}>
+            <Text style={styles.buttonText}> Logout </Text>
+          </TouchableOpacity>
         </View>
-    );
+      ) : (
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            setSection("Register");
+          }}
+        >
+          <Text style={styles.buttonText}> Register </Text>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    topbar: {
-        flexDirection: "row",
-    },
-    logo: {
-        flex: 1,
-    },
-    gap: {
-        flex: 3,
-    },
-    login: {
-        flex: 1,
-    },
-    button: {
-        flex: 1,
-        backgroundColor: "#4a90e2",
-        paddingHorizontal: 2,
-        borderRadius: 2,
-    },
-    buttonText: {
-        color: "#fff",
-        fontSize: 10,
-    },
+  topbar: {
+    flexDirection: "row",
+  },
+  logo: {
+    flex: 1,
+  },
+  gap: {
+    flex: 3,
+  },
+  login: {
+    flex: 1,
+  },
+  button: {
+    flex: 1,
+    backgroundColor: "#4a90e2",
+    paddingHorizontal: 2,
+    borderRadius: 2,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 10,
+  },
 });
 
 export default TopBar;
